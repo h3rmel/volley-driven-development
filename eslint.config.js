7b871bc87ebc5a7d963bb22b3d372d6d1914c683
h3rmel <isaach3rmel@gmail.com>
2025-09-12 14:25:37 -0300

    config eslint and integrates with prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,58 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      /**
+       * Default rules
+       * @see https://eslint.org/docs/latest/rules/
+       */
+      // Disable console.log to encourage more explicit logging
+      'no-console': [
+        'warn',
+        {
+          allow: [
+            'warn',
+            'error',
+            'info',
+            'dir',
+            'table',
+            'assert',
+            'count',
+            'time',
+            'timeLog',
+            'trace',
+            'groupCollapsed',
+            'groupEnd',
+          ],
+        },
+      ],
+      'no-alert': 'error',
+      'no-template-curly-in-string': 'error',
+      'prefer-template': 'warn',
+      'no-implicit-coercion': 'warn',
+      'require-await': 'warn',
+      'no-plusplus': ['warn', { allowForLoopAfterthoughts: true }],
+      'no-restricted-imports': 'off',
+      'padding-line-between-statements': 'warn',
+    },
   },
-])
+);
